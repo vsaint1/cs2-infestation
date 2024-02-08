78f61f8ad44f40f58ea9c7a943671d01d7c26e37
vsaint1 <vsantos1.dev@gmail.com>
2024-02-08 02:34:16 -0300

    macro fmt helper

--- Usermode/utils/helper_macro.h
+++ Usermode/utils/helper_macro.h
@@ -1,17 +1,16 @@
 #pragma once
+#include <iostream>
 
 #define GAME_NAME "cs2.exe"
 #define MODULE_NAME "client.dll"
 
 #ifdef _DEBUG
-#define LOG(fmt, ...)\
-{\
-const auto filename = strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__;\
-printf("[%s:%d] - " fmt "\n", filename, __LINE__, __VA_ARGS__);\
-}
+#define LOG(fmt, ...) \
+    do { \
+        const auto filename = strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__; \
+        std::cout << "[infestation:" << filename << ":" << __LINE__ << "] - " << fmt << "\n"; \
+    } while (0)
 #else
-#define LOG(fmt, ...)\
-{\
-printf("[cs2-infestation] - " fmt "\n", __VA_ARGS__);\
-}
+#define LOG(fmt, ...)                                                                                                                                                                                  \
+  { printf("[infestation] - " fmt "\n", __VA_ARGS__); }
 #endif
