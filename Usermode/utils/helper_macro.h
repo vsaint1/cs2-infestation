7f9fbb47f1ae9931f773600f10025c900d4e108d
vsaint1 <vsantos1.dev@gmail.com>
2024-02-09 02:18:10 -0300

    reverted macro fmt

--- Usermode/utils/helper_macro.h
+++ Usermode/utils/helper_macro.h
@@ -5,11 +5,11 @@
 #define MODULE_NAME "client.dll"
 
 #ifdef _DEBUG
-#define LOG(fmt, ...) \
-    do { \
-        const auto filename = strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__; \
-        std::cout << "[infestation:" << filename << ":" << __LINE__ << "] - " << fmt << "\n"; \
-    } while (0)
+#define LOG(fmt, ...)\
+{\
+const auto filename = strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__;\
+printf("[infestation-%s:%d] - " fmt "\n", filename, __LINE__, __VA_ARGS__);\
+}
 #else
 #define LOG(fmt, ...)                                                                                                                                                                                  \
   { printf("[infestation] - " fmt "\n", __VA_ARGS__); }
