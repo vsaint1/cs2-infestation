07175878ec98079b8c8aa9ac4409fea13e5c0867
vsaint1 <vsantos1.dev@gmail.com>
2024-02-12 02:57:19 -0300

    feat: better offsets usage

--- Usermode/features/globals.h
+++ Usermode/features/globals.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Windows.h>
+#include "../../external/imgui/imgui.h"
 
 HWND entry_hwnd = NULL;
 int processid = 0;
@@ -8,8 +9,8 @@ RECT Rect;
 ULONG64 client = 0;
 uintptr_t global_pawn = 0;
 
-static ULONG width = GetSystemMetrics(SM_CXSCREEN);
-static ULONG height = GetSystemMetrics(SM_CYSCREEN);
+static ULONG width = GetSystemMetrics(SM_CXSCREEN); 
+static ULONG height = GetSystemMetrics(SM_CYSCREEN); 
 
 namespace hotkeys {
 	int aimkey;
@@ -22,16 +23,102 @@ namespace offsets {
 	std::ptrdiff_t dwLocalPlayerPawn = 0x17262E8;
 	std::ptrdiff_t dwGameEntitySystem_getHighestEntityIndex = 0x1510;
 	std::ptrdiff_t dwViewMatrix = 0x19102B0;
-	std::ptrdiff_t dwPawnHealth = 0x7F0;    // m_iPawnHealth = 0x7F8; // uint32_t
-	std::ptrdiff_t dwPlayerPawn = 0x7E4;    // m_hPlayerPawn client.dll.rs
-	std::ptrdiff_t dwSanitizedName = 0x640; //  m_iszPlayerName = 0x640; // char[128]
-	std::ptrdiff_t m_bDormant = 0xE7;
-	std::ptrdiff_t m_iTeamNum = 0x3CB;
-	std::ptrdiff_t m_vecOrigin = 0x127C;          // m_vOldOrigin client.dll.rs
-	std::ptrdiff_t m_iHealth = 0x334;             // m_iHealth client.dll.rs
-	std::ptrdiff_t bSpottedByMask = 0x1698 + 0xC; // entitySpottedState + bSpottedByMask
-	std::ptrdiff_t m_iszPlayerName = 0x638;       // char[128]
-	std::ptrdiff_t iIDEntIndex = 0x153C;          // m_iIDEntIndex = 0x153C;
+
+	namespace CBasePlayerController { // C_BaseEntity
+		 std::ptrdiff_t m_nFinalPredictedTick = 0x558; // int32_t
+		 std::ptrdiff_t m_CommandContext = 0x560; // C_CommandContext
+		 std::ptrdiff_t m_nInButtonsWhichAreToggles = 0x5F8; // uint64_t
+		 std::ptrdiff_t m_nTickBase = 0x600; // uint32_t
+		 std::ptrdiff_t m_hPawn = 0x604; // CHandle<C_BasePlayerPawn>
+		 std::ptrdiff_t m_hPredictedPawn = 0x608; // CHandle<C_BasePlayerPawn>
+		 std::ptrdiff_t m_nSplitScreenSlot = 0x60C; // CSplitScreenSlot
+		 std::ptrdiff_t m_hSplitOwner = 0x610; // CHandle<CBasePlayerController>
+		 std::ptrdiff_t m_hSplitScreenPlayers = 0x618; // CUtlVector<CHandle<CBasePlayerController>>
+		 std::ptrdiff_t m_bIsHLTV = 0x630; // bool
+		 std::ptrdiff_t m_iConnected = 0x634; // PlayerConnectedState
+		 std::ptrdiff_t m_iszPlayerName = 0x638; // char[128]
+		 std::ptrdiff_t m_steamID = 0x6C0; // uint64_t
+		 std::ptrdiff_t m_bIsLocalPlayerController = 0x6C8; // bool
+		 std::ptrdiff_t m_iDesiredFOV = 0x6CC; // uint32_t
+	};
+
+	namespace CCSPlayerController { // CBasePlayerController
+		 std::ptrdiff_t m_hPlayerPawn = 0x7E4; // CHandle<C_CSPlayerPawn>
+		 std::ptrdiff_t m_hObserverPawn = 0x7E8; // CHandle<C_CSObserverPawn>
+		 std::ptrdiff_t m_bPawnIsAlive = 0x7EC; // bool
+		 std::ptrdiff_t m_iPawnHealth = 0x7F0; // uint32_t
+		 std::ptrdiff_t m_iPawnArmor = 0x7F4; // int32_t
+		 std::ptrdiff_t m_bPawnHasDefuser = 0x7F8; // bool
+		 std::ptrdiff_t m_bPawnHasHelmet = 0x7F9; // bool
+	};
+
+	namespace C_CSPlayerPawnBase { // C_BasePlayerPawn
+
+		 std::ptrdiff_t m_iIDEntIndex = 0x15A4; // CEntityIndex
+		 std::ptrdiff_t m_bGunGameImmunity = 0x141C; // bool
+		 std::ptrdiff_t m_pClippingWeapon = 0x1308; // C_CSWeaponBase*
+	};
+
+	namespace C_CSPlayerPawn {
+		 std::ptrdiff_t m_entitySpottedState = 0x1698; // EntitySpottedState_t
+
+	};
+
+	namespace EntitySpottedState_t {
+		 std::ptrdiff_t m_bSpotted = 0x8; // bool
+		 std::ptrdiff_t m_bSpottedByMask = 0xC; // uint32_t[2]
+	};
+
+	namespace C_BasePlayerPawn {
+		 std::ptrdiff_t m_vOldOrigin = 0x127C; // Vector
+
+	};
+
+	namespace C_BaseEntity {
+		 std::ptrdiff_t m_iTeamNum = 0x3CB; // uint8_t
+		 std::ptrdiff_t m_iHealth = 0x334; // int32_t
+		 std::ptrdiff_t m_pGameSceneNode = 0x318; // CGameSceneNode*
+	};
+
+	namespace CSkeletonInstance {
+		 std::ptrdiff_t m_modelState = 0x160; // CModelState
+
+	};
+
+
+	namespace CGameSceneNode {
+
+		 std::ptrdiff_t m_vecOrigin = 0x80; // CNetworkOriginCellCoordQuantizedVector
+
+	};
+
+	namespace CEntityInstance {
+		 std::ptrdiff_t m_pEntity = 0x10; // CEntityIdentity*
+	};
+
+	namespace CEntityIdentity {
+		 std::ptrdiff_t m_designerName = 0x20; // CUtlSymbolLarge
+	}
+
+	namespace C_BaseCSGrenadeProjectile {
+		 std::ptrdiff_t m_vInitialPosition = 0x10C0; // Vector
+		 std::ptrdiff_t m_vInitialVelocity = 0x10CC; // Vector
+		 std::ptrdiff_t m_nBounces = 0x10D8; // int32_t
+		 std::ptrdiff_t m_arrTrajectoryTrailPoints = 0x1120; // CUtlVector<Vector>
+
+	};
+
+	namespace C_SmokeGrenadeProjectile {
+		 std::ptrdiff_t m_nSmokeEffectTickBegin = 0x1160; // int32_t
+		 std::ptrdiff_t m_bDidSmokeEffect = 0x1164; // bool
+		 std::ptrdiff_t m_nRandomSeed = 0x1168; // int32_t
+		 std::ptrdiff_t m_vSmokeColor = 0x116C; // Vector
+		 std::ptrdiff_t m_vSmokeDetonationPos = 0x1178; // Vector
+		 std::ptrdiff_t m_VoxelFrameData = 0x1188; // CUtlVector<uint8_t>
+		 std::ptrdiff_t m_bSmokeVolumeDataReceived = 0x11A0; // bool
+		 std::ptrdiff_t m_bSmokeEffectSpawned = 0x11A1; // bool
+	};
+
 } // namespace offsets
 
 namespace settings {
@@ -86,7 +173,10 @@ namespace settings {
 
 
 	}
+	namespace colors {
 
+		ImColor player_snaplines = ImColor(255, 255, 255, 255);
+	};
 } // namespace settings
 
 enum EBone : int {
@@ -104,7 +194,7 @@ enum EBone : int {
 	LeftKnee = 26,
 	RightFoot = 24,
 	LeftFoot = 27,
-	Center= 29,
+	Center = 29,
 
 };
 
