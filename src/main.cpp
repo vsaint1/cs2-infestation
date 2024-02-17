41f8c3b390b2ec688c5cbe819438281abbca843e
vsaint1 <vsantos1.dev@gmail.com>
2024-02-17 12:30:29 -0300

    feat:  testing sample

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,11 @@
 int main() {
   memory.attach();
 
-  printf("PID %d\n", memory.get_pid());
+  SPDLOG_INFO("PID {} \n", memory.get_pid());
 
   auto local_pawn = memory.find_pattern("client.dll", "48 8D 05 ? ? ? ? C3 CC CC CC CC CC CC CC CC 48 83 EC ? 8B 0D")->rip().add(0x138);
   auto [address, size] = memory.get_module_info("client.dll");
-  printf("MODULE_BASE %p", address.value());
-  printf("LOCAL_PAWN %p", local_pawn);
+  SPDLOG_INFO("MODULE_BASE: {}", address.value());
 
   while (true) {
     auto lp = memory.readv<uintptr_t>(local_pawn.get_address());
@@ -20,7 +19,7 @@ int main() {
       continue;
 
     int health = memory.readv<int>(lp + 0x334);
-    printf("HEALTH %d \n", health);
+    SPDLOG_INFO("HEALTH: {} \n", health);
   }
 
   return EXIT_SUCCESS;
