0e6cf1eebd6bd7eb5444dff8a32d2182afa22c58
vsaint1 <vsantos1.dev@gmail.com>
2024-02-17 12:23:50 -0300

    SPD_LOG tracer

--- src/utils/macros.h
+++ src/utils/macros.h
@@ -9,6 +9,16 @@
 #include <string>
 #include <string_view>
 
+
+#ifdef _DEBUG
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
+#include <spdlog/spdlog.h>
+#else
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_OFF
+#include <spdlog/spdlog.h>
+
+#endif
+
 bool compare(std::string_view str1, std::string_view str2);
 
 #define EQUALS_IGNORE_CASE(str1, str2) compare(str1,str2)
