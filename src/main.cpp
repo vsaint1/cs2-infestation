8585ac5264639cabcd81420a2726e4ff6077c159
vsaint1 <vsantos1.dev@gmail.com>
2024-02-22 02:33:42 -0300

    implemented on main

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,47 @@
 //
 // Created by vsantos1 on 2/16/2024.
 //
-#include "memory/memory.hpp"
-#include <iostream>
+#define SDL_MAIN_HANDLED
+#include "sdk/entity_list.hpp"
+#include "window/window_manager.h"
 
 int main() {
   memory.attach();
 
   SPDLOG_INFO("PID {} \n", memory.get_pid());
+  auto entity_list = memory.find_pattern("client.dll", "48 8B 0D ? ? ? ? 48 89 7C 24 ? 8B FA C1 EB")->rip();
 
   auto local_pawn = memory.find_pattern("client.dll", "48 8D 05 ? ? ? ? C3 CC CC CC CC CC CC CC CC 48 83 EC ? 8B 0D")->rip().add(0x138);
+  SPDLOG_INFO("LOCAL_PLAYER: {:#04x}", local_pawn.get_address());
   auto [address, size] = memory.get_module_info("client.dll");
   SPDLOG_INFO("MODULE_BASE: {}", address.value());
 
-  while (true) {
-    auto lp = memory.readv<uintptr_t>(local_pawn.get_address());
-    if (!lp)
-      continue;
+  WindowManager manager;
 
-    int health = memory.readv<int>(lp + 0x334);
-    SPDLOG_INFO("HEALTH: {} \n", health);
+  if (!manager.create()) {
+    manager.cleanup();
+    MessageBox(0, "Failed while trying to initialize modules", "Error", MB_ICONERROR);
   }
+
+  manager.make_window_transparent(manager.m_window, RGB(255, 0, 255));
+
+  int x, y = 0;
+
+  SDL_Event event;
+  while (!manager.should_close(&event)) {
+
+    manager.draw_rect((manager.m_width - 100) / 2, (manager.m_height - 100) / 2, 100, 100, {255, 0, 255, 255});
+    SDL_RenderPresent(manager.m_renderer);
+
+    auto cursor = SDL_GetGlobalMouseState(&x, &y);
+
+    SPDLOG_INFO("MOUSE_X: {} MOUSE_Y {}", x, y);
+
+    if (GetAsyncKeyState(VK_LBUTTON) & 0x8000) // TODO: change to SDL events
+      manager.send_events(manager.m_hwnd, x, y);
+  }
+
+  manager.cleanup();
 
   return EXIT_SUCCESS;
 }
