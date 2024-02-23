8ad35165fdaaddb0e5112a7b7eb90c4479021d98
vsaint1 <vsantos1.dev@gmail.com>
2024-02-22 23:28:20 -0300

    fix:  globals.h header guard

--- src/features/globals.h
+++ src/features/globals.h
@@ -2,6 +2,12 @@
  *   Copyright (c) 2024 vsantos1
  *   All rights reserved.
  */
+
+#ifndef FEATURES_GLOBALS_H_
+#define FEATURES_GLOBALS_H_
+
+
+
 #include <SDL2/SDL_pixels.h>
 
 inline uintptr_t local_player;
@@ -17,3 +23,5 @@ inline SDL_Color gray = {200, 200, 200};
 } // namespace colors
 
 } // namespace settings
+
+#endif // FEATURES_GLOBALS_H_
