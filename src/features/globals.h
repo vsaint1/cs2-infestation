be585e831effa82e0ad0f0a9ed561b81f7f62736
vsaint1 <vsantos1.dev@gmail.com>
2024-02-22 23:42:24 -0300

    C_PlantedC4 offsets

--- src/features/globals.h
+++ src/features/globals.h
@@ -6,14 +6,25 @@
 #ifndef FEATURES_GLOBALS_H_
 #define FEATURES_GLOBALS_H_
 
-
-
+#include <cstddef>
 #include <SDL2/SDL_pixels.h>
 
 inline uintptr_t local_player;
 
 inline uintptr_t client;
 
+namespace offsets {
+namespace C_PlantedC4 {                               // CBaseAnimGraph
+constexpr std::ptrdiff_t m_nBombSite = 0xEDC;         // int32_t
+constexpr std::ptrdiff_t m_flC4Blow = 0xF08;          // GameTime_t
+constexpr std::ptrdiff_t m_bBeingDefused = 0xF14;     // bool
+constexpr std::ptrdiff_t m_flDefuseCountDown = 0xF28; // GameTime_t
+constexpr std::ptrdiff_t m_bBombDefused = 0xF2C;      // bool
+
+};                                                    // namespace C_PlantedC4
+
+namespace C_BaseEntity {};
+}; // namespace offsets
 namespace settings {
 namespace colors {
 
