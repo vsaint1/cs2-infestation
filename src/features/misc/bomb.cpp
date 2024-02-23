12f4b609b327d959ddba380aa36d9c603923135d
vsaint1 <vsantos1.dev@gmail.com>
2024-02-22 23:42:38 -0300

    removed unrelated bomb draw

--- src/features/misc/bomb.cpp
+++ src/features/misc/bomb.cpp
@@ -2,28 +2,27 @@
 
 void misc::bomb_timer(WindowManager &manager) {
 
+  global_vars = memory.readv<GlobalVarsBase>(memory.readv<uintptr_t>(client + 0x171CE70));
+
   auto temp_c4 = memory.readv<uintptr_t>(client + 0x191B508);
   auto plantedC4 = memory.readv<uintptr_t>(temp_c4);
 
   auto bombplanted = memory.readv<bool>(client + 0x191B508 - 0x8);
 
-  manager.draw_text(memory.read_str(global_vars.m_currentMapName), 300, 300, settings::colors::gray);
   if (!bombplanted)
     manager.draw_text("Waiting bomb", 36, 702, settings::colors::gray);
 
-  manager.draw_text(std::to_string(global_vars.m_currentMapName), 36, 800, settings::colors::gray);
-
   if (bombplanted) {
-    auto bomb_site = memory.readv<int>(plantedC4 + 0xedc); // m_nBombSite
+    auto bomb_site = memory.readv<int>(plantedC4 + offsets::C_PlantedC4::m_nBombSite); 
 
-    bool bomb_defused = memory.readv<bool>(plantedC4 + 0xf2c); // m_bBombDefused
+    bool bomb_defused = memory.readv<bool>(plantedC4 + offsets::C_PlantedC4::m_bBombDefused); 
 
-    auto temp_bomb_time = memory.readv<float>(plantedC4 + 0xf08); // m_flC4Blow
+    auto temp_bomb_time = memory.readv<float>(plantedC4 + offsets::C_PlantedC4::m_flC4Blow); 
     auto fl_bomb_time = temp_bomb_time - global_vars.m_flcurrentTime;
 
-    bool being_defused = memory.readv<bool>(plantedC4 + 0xf14); // m_bBeingDefused
+    bool being_defused = memory.readv<bool>(plantedC4 + offsets::C_PlantedC4::m_bBeingDefused); 
 
-    float temp_defuse_time = memory.readv<float>(plantedC4 + 0xf28); // m_flDefuseCountDown
+    float temp_defuse_time = memory.readv<float>(plantedC4 + offsets::C_PlantedC4::m_flDefuseCountDown);
     auto fl_defuse_time = temp_defuse_time - global_vars.m_flcurrentTime;
 
     if (!bomb_defused) {
@@ -35,5 +34,6 @@ void misc::bomb_timer(WindowManager &manager) {
       std::string defuse_timer(fmt::format("Bomb being defused, time remaining {:.2f}", fl_defuse_time));
       manager.draw_text(defuse_timer, 36, 730, settings::colors::red);
     }
+
   }
 }
