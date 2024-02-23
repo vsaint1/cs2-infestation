fde008819cbe2f211dea6f7c92858ea05d491790
vsaint1 <vsantos1.dev@gmail.com>
2024-02-22 23:08:34 -0300

    global offsets and variables

---
+++ src/features/globals.h
@@ -0,0 +1,19 @@
+/*
+ *   Copyright (c) 2024 vsantos1
+ *   All rights reserved.
+ */
+#include <SDL2/SDL_pixels.h>
+
+inline uintptr_t local_player;
+
+inline uintptr_t client;
+
+namespace settings {
+namespace colors {
+
+inline SDL_Color red = {255, 0, 0, 255};
+inline SDL_Color green = {0, 255, 0, 255};
+inline SDL_Color gray = {200, 200, 200};
+} // namespace colors
+
+} // namespace settings
