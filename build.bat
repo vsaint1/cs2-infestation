d53a86d122738951d1c987232d3f95c01630f698
vsaint1 <vsantos1.dev@gmail.com>
2024-02-23 02:06:18 -0300

    build script in release mode

--- build.bat
+++ build.bat
@@ -1,8 +1,12 @@
 @ECHO off
 
+ECHO Detecting conan profile
+
+conan profile detect --force
+
 ECHO Installing depencencies
 
-conan install . --build=missing
+conan install . --build=missing --settings=build_type=Release
 
 ECHO Building in progress
 
