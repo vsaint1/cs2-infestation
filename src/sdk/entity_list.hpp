d52a53956c7db4a83bcd6116a4e65cf97a6311d3
vsaint1 <vsantos1.dev@gmail.com>
2024-02-24 02:11:43 -0300

    reading entity_list

---
+++ src/sdk/entity_list.hpp
@@ -0,0 +1,34 @@
+/*
+ *   Copyright (c) 2024 vsantos1
+ *   All rights reserved.
+ */
+#ifndef SDK_ENTITY_LIST_H_
+#define SDK_ENTITY_LIST_H_
+
+#include "../memory/memory.hpp"
+
+class EntityList {
+
+public:
+  static EntityList *get();
+
+  template <typename T> T get(const int i) {
+    const auto list_entry = memory.readv<uintptr_t>(this + 8LL * ((i & 0x7FFF) >> 9) + 16);
+
+    if (!list_entry)
+      return 0;
+
+    const auto controller = list_entry + 120LL * (i & 0x1FF);
+
+    if (!controller)
+      return 0;
+
+    return memory.readv<T>(controller);
+  }
+
+
+};
+
+inline EntityList *entity_list{};
+
+#endif // SDK_ENTITY_LIST_H_
