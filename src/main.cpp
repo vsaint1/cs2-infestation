dbaea923859d455004644210c35f22e3de23e6be
vsaint1 <vsantos1.dev@gmail.com>
2024-02-24 02:12:29 -0300

    reading entity_list entry

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,8 @@ int main() {
   memory.attach();
 
   SPDLOG_INFO("PID {} \n", memory.get_pid());
-  auto entity_list = memory.find_pattern("client.dll", "48 8B 0D ? ? ? ? 48 89 7C 24 ? 8B FA C1 EB")->rip();
+  entity_list = memory.find_pattern("client.dll", "48 8B 0D ? ? ? ? 48 89 7C 24 ? 8B FA C1 EB")->rip().as<EntityList *>();
+
 
   auto local_pawn = memory.find_pattern("client.dll", "48 8D 05 ? ? ? ? C3 CC CC CC CC CC CC CC CC 48 83 EC ? 8B 0D")->rip().add(0x138);
   SPDLOG_INFO("LOCAL_PLAYER: {:#04x}", local_pawn.get_address());
@@ -28,8 +29,28 @@ int main() {
   SDL_Event event;
   while (!manager.should_close(&event)) {
 
-    if (!local_player)
-      continue;
+    for (auto i = 1; i < 2048; i++) {
+
+     auto e_list = EntityList::get();
+      if (!e_list)
+        continue;
+      const auto entity = e_list->get<uintptr_t>(i);
+      if (!entity)
+        continue;
+
+      uintptr_t entity_identity = memory.readv<uintptr_t>(entity + 0x10);
+      if (!entity_identity)
+        continue;
+
+      uintptr_t designer_name = memory.readv<uintptr_t>(entity_identity + 0x20);
+      if (!designer_name)
+        continue;
+
+      std::string clazz_name = memory.read_str(designer_name);
+
+      SPDLOG_INFO("ENTITY: {} name {}", entity, clazz_name);
+
+    }
 
     manager.update();
     manager.make_window_transparent(manager.m_window, RGB(0, 0, 0));
@@ -40,8 +61,7 @@ int main() {
       manager.draw_rect((manager.m_width - 100) / 2, (manager.m_height - 100) / 2, 100, 100, settings::colors::green);
 
     misc::bomb_timer(manager);
-    int health = memory.readv<int>(local_player + 0x334);
-    manager.draw_text(std::to_string(health), 350, 350, settings::colors::green);
+
     manager.render();
 
     // SPDLOG_INFO("MOUSE_X: {} MOUSE_Y {}", x, y);
