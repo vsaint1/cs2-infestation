7f0e87b80d6eefb403747ce1fcb2013e16bbfb01
vsaint1 <vsantos1.dev@gmail.com>
2024-03-02 03:22:19 -0300

    print macro

--- driver/utils/macros.h
+++ driver/utils/macros.h
@@ -2,7 +2,7 @@
 
 
 #ifdef _DEBUG
-#define printf(...) DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "[INFESTATION] - " __VA_ARGS__ "\n")
+#define printf(...) DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "[INFESTATION] - " __VA_ARGS__)
 #else
 #define printf(...)
 #endif
