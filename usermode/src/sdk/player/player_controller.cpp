21b3ddaa412d5845788a5ab1977272451e4ac04f
vsaint1 <vsantos1.dev@gmail.com>
2024-03-04 00:56:30 -0300

    player_controller.cpp defs

--- usermode/src/sdk/player/player_controller.cpp
+++ usermode/src/sdk/player/player_controller.cpp
@@ -4,12 +4,14 @@
  */
 #include "player_controller.h"
 
+#include <utility>
 
-bool PlayerController::same_team(int &local_team,int &enemy_team) {
+
+bool PlayerController::same_team(int local_team, int enemy_team) {
     return local_team == enemy_team;
 }
 
-bool PlayerController::is_localplayer(uintptr_t &local_pawn, uintptr_t &entity_pawn) {
+bool PlayerController::is_localplayer(uintptr_t local_pawn, uintptr_t entity_pawn) {
     return entity_pawn == local_pawn;
 
 }
@@ -26,6 +28,56 @@ Vector3 PlayerController::get_bone_pos_2d(uintptr_t &bonearray, int &bone_id) {
 Vector3 PlayerController::get_bone_pos_2d(uintptr_t bonearray, EBone bone) {
     return memory.readv<Vector3>(bonearray + static_cast<int>(bone) * 32).world_to_screen(local_viewmatrix);
 
+}
+
+PlayerController::PlayerController(uintptr_t pawn, int idx, EntityType type, int mHealth, bool mVisible,
+                                   bool mHasGunImmunity, const Vector3 &mCurrentPos, uintptr_t mBoneArray,
+                                   int mCurrentTeam, std::string mPlayerName, std::string mWeaponName)
+        : BaseEntity(pawn, idx, type), m_health(mHealth), m_visible(mVisible), m_hasGunImmunity(mHasGunImmunity),
+          m_currentPos(mCurrentPos), m_boneArray(mBoneArray), m_currentTeam(mCurrentTeam), m_playerName(std::move(mPlayerName)),
+          m_weaponName(std::move(mWeaponName)) {}
+
+
+float PlayerController::get_distance() {
+    return this->m_currentPos.distance(local_player + offsets::C_BasePlayerPawn::m_vOldOrigin) / 100;
+}
+
+int PlayerController::get_m_health() const {
+    return m_health;
+}
+
+bool PlayerController::is_m_visible() const {
+    return m_visible;
+}
+
+bool PlayerController::is_m_has_gun_immunity() const {
+    return m_hasGunImmunity;
+}
+
+const Vector3 &PlayerController::get_m_current_pos() const {
+    return m_currentPos;
+}
+
+uintptr_t PlayerController::get_m_bone_array() const {
+    return m_boneArray;
+}
+
+int PlayerController::get_m_current_team() const {
+    return m_currentTeam;
+}
+
+std::string PlayerController::get_m_player_name() const {
+    if (this->m_playerName.empty())
+        return "unknown_player";
+    else
+        return this->m_playerName;
+}
+
+std::string PlayerController::get_m_weapon_name() const {
+    if (this->m_weaponName.find("weapon_") == std::string::npos)
+        return "unknown_weapon";
+    else
+        return this->m_weaponName.substr(7);
 }
 
 
--- usermode/src/sdk/player/player_controller.h
+++ usermode/src/sdk/player/player_controller.h
@@ -6,6 +6,7 @@
 #ifndef SDK_PLAYER_CONTROLLER_HPP_
 #define SDK_PLAYER_CONTROLLER_HPP_
 
+#include <ostream>
 #include "../base_entity.hpp"
 
 enum EBone : uint8_t {
@@ -29,18 +30,47 @@ enum EBone : uint8_t {
 
 class PlayerController : public BaseEntity {
 
+    int m_health;
+    bool m_visible;
+    bool m_hasGunImmunity;
+    Vector3 m_currentPos;
+    uintptr_t m_boneArray;
+    int m_currentTeam;
+    std::string m_playerName;
+    std::string m_weaponName;
 public:
 
+    float get_distance();
 
-    static Vector3 get_bone_pos_2d(uintptr_t bonearray,EBone bone);
+    int get_m_health() const;
 
-    static Vector3 get_bone_pos_2d(uintptr_t &bonearray,int &bone_id);
+    bool is_m_visible() const;
 
-    static bool is_localplayer(uintptr_t &local_pawn, uintptr_t &entity_pawn);
+    bool is_m_has_gun_immunity() const;
+
+    const Vector3 &get_m_current_pos() const;
+
+    uintptr_t get_m_bone_array() const;
+
+    int get_m_current_team() const;
+
+     std::string get_m_player_name() const;
+
+     std::string get_m_weapon_name() const;
+
+    PlayerController(uintptr_t pawn, int idx, EntityType type, int mHealth, bool mVisible, bool mHasGunImmunity,
+                     const Vector3 &mCurrentPos, uintptr_t mBoneArray, int mCurrentTeam, std::string mPlayerName,
+                     std::string mWeaponName);
+
+    static Vector3 get_bone_pos_2d(uintptr_t bonearray, EBone bone);
+
+    static Vector3 get_bone_pos_2d(uintptr_t &bonearray, int &bone_id);
+
+    static bool is_localplayer(uintptr_t local_pawn, uintptr_t entity_pawn);
 
     static bool is_dead(int &entity_health);
 
-    static bool same_team(int &local_team, int &enemy_team);
+    static bool same_team(int local_team, int enemy_team);
 
 };
 
